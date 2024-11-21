8c3ea565b68f27051596ddf5fed205be1d86abbd
Dipen Pradhan <dipenp@google.com>
2024-11-21 14:50:07 -0800

    Add dynamic RROs, set resolution in display settings
    Add makefile to include dynamic RROs
    Update display settings to use 1920x720
    
    Bug: 380158261
         379765743
    Test: Enable RRO and verify that the value is set as expected:
          adb shell cmd overlay enable --user 0 com.android.car.driverui.harclient.grpc.rro
          adb shell cmd overlay lookup --verbose --user 0 com.android.car.driverui com.android.car.driverui:integer/har_client_type
          Repeat for com.android.car.driverui.harclient.tcp.rro
          and for com.android.car.driverui.harclient.sdv.rro
    Flag: EXEMPT - Used in internal cuttlefish development target
    
    Change-Id: I61ee244b9ad22dd396b963c8c5298a64117a6625

---
+++ service/ivi/driverui_har_client_rros.mk
@@ -0,0 +1,9 @@
+# Copyright 2024 Google LLC
+
+# driverui_har_client_rros
+# This file adds dynamic RROs to set har_client_type in DriverUI
+
+PRODUCT_PACKAGES += \
+    DriverUIHarClientGrpcRRO \
+    DriverUIHarClientTcpRRO \
+    DriverUIHarClientSdvRRO \
