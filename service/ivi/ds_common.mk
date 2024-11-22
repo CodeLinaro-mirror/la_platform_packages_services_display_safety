616b46c1e3afa52e8f94e63f069234466b78383f
Dipen Pradhan <dipenp@google.com>
2024-11-22 10:33:00 -0800

    Add ds_common.mk, add AutoVoipTestApp
    ds_common.mk is intended to share makefile contents for _ds devices
    Add AutoVoipTestApp to test calling features in DriverUI
    
    Bug: 379963438
         379183768
    Test: Build sdv_ivi_arm64_ds, cf_x86_64_auto_ds and flash/launch it
          Verify DriverUI, SdvCameraPreviewApp and config
    Flag: EXEMPT - internal development only, cuttlefish based device
    Change-Id: I12a28f4f0e0f1f7795fe0fd5b5c56bf6467df429

---
+++ service/ivi/ds_common.mk
@@ -0,0 +1,28 @@
+# Copyright 2024 Google LLC
+#
+# ds_common
+# Adds Apps, RROs and Soong configs required for Display Safety
+
+# Set cluster display settings
+PRODUCT_COPY_FILES += \
+    vendor/google/display_safety/service/ivi/files/vendor/etc/display_settings.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display_settings.xml
+
+# TODO: b/380354610
+# Set default RRO config
+#PRODUCT_COPY_FILES += \
+#    vendor/google/display_safety/service/ivi/files/product/overlay/config/config.xml:$(TARGET_COPY_OUT_PRODUCT)/overlay/config/config.xml
+
+# Displaycompat
+$(call inherit-product-if-exists, vendor/auto/embedded/products/displaycompat/product.mk)
+
+# Include DriverUI app and enable it using RROs
+$(call inherit-product, packages/services/Car/car_product/driverui/driverui_app_and_rros.mk)
+
+# Include dynamic RROs to set HAR client config for DriverUI
+$(call inherit-product, vendor/google/display_safety/service/ivi/driverui_har_client_rros.mk)
+
+# Include SdvCameraPreviewApp, related RROs and configs
+$(call inherit-product, vendor/google/display_safety/service/ivi/sdv_camera_preview.mk)
+
+PRODUCT_PACKAGES += \
+    AutoVoipTestApp
