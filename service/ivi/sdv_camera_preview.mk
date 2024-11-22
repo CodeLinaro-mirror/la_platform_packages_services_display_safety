0934ec8cfdf3889899d8c4df56e1066ed309d387
Changyeon Jo <changyeon@google.com>
2024-11-22 23:04:10 +0000

    Enable a mock EVS HAL on CF-based virtual devices for display safety
    Bug: 380467672
    Test: Run EVS applications on sdv_ivi_arm64_ds target
    Flag: EXEMPT makefile change only
    Change-Id: I90f860bc6b70380b963ca4b69f5d1d692f2adcc6

--- service/ivi/sdv_camera_preview.mk
+++ service/ivi/sdv_camera_preview.mk
@@ -13,3 +13,8 @@ PRODUCT_PACKAGES += SdvCameraPreviewAppPrebuilt
 SOONG_CONFIG_NAMESPACES += sdvcamerapreviewapp
 SOONG_CONFIG_sdvcamerapreviewapp += enabled
 SOONG_CONFIG_sdvcamerapreviewapp_enabled := true
+
+# TODO(b/380467672): Temporarily, we are using a mock EVS HAL and its emulated cameras
+#                    on CF-based targets for display-safety.
+ENABLE_MOCK_EVSHAL := true
+ENABLE_EVS_SAMPLE := false
